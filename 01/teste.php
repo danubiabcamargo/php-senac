af1a7c14d7d064da3bb2eab826c2a77788613a1c
Danubia@senac <danubiabcamargo@gmail.com>
2024-03-20 22:12:37 -0300

    conhecendo o arquivo php

---
+++ 01/teste.php
@@ -0,0 +1,27 @@
+
+<?php
+//marca onde inicia e termina o php 
+    echo "Hello World!" . "<br>";
+
+    # Declaração de variáveis
+     $nome = "Pedro";
+     $sobrenome = "Fausta";
+     $nome_completo = $nome. " ". $sobrenome;
+// variavel é declaro direto e marcado com $//
+
+    #echo $nome;
+    #echo $sobrenome;
+    echo $nome. " ". $sobrenome . "<br>";
+    echo $nome_completo . "<br>" . "<br>" . "<br>";
+    echo "$nome tem $idade anos de idade" . "<br>";
+
+// variaveis numericas
+    $idade = 25;
+    $ano = 2024;
+
+    echo  "Idade é: " . $idade . "<br>";
+    echo  "Data de Nascimento: " . $ano . "<br>";
+
+
+
+?>
