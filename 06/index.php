ef5cc9d1989270d4a6adaf7bf12bded5c2030103
Danubia@senac <danubiabcamargo@gmail.com>
2024-03-27 20:46:41 -0300

    06

--- 06/index.php
+++ 06/index.php
@@ -19,7 +19,58 @@
     echo 'Verificação se $array é do tipo array: ' . (is_array($array)? "Sim" : "Não") . "<br>";
 
 
+    /*  Conversão de Tipos de Dados */
+    $numero_string = "456";
+    $numero_convertido = (int)$numero_string;
 
+    echo "Número convertida de string para int: $numero_convertido <br>";
+
+    /* Demais conversões: */
+    $numero = 456;
+    $string_numero = (string)$numero;
+
+    $bool_string = "true";
+    $bool_convertido = (bool)$bool_string;
+
+    $array_string = "1,2,3";
+    $array_convertido = explode(",", $array_string);
+
+    echo "Números em uma string antes da conversão : $array_string <br>";
+    echo "Array convertido de string para array: ";
+    print_r($array_convertido);
+    echo "<br>";
+
+    # exemplo adicional 1 - conversão para array
+    $hora = "20:02:56"; //hora lida de um log
+    $array_hora_minuto = explode(":", $hora);
+    echo "O evento ocorreu às $array_hora_minuto[0] horas, $array_hora_minuto[1] minutos, $array_hora_minuto[2] segundos.";
+    echo "<br>";
+    
+    # exemplo adicional 2 - conversão para array
+    $contatos = "Aecio-Nerildo-JS-Celso-Luana";
+    echo "A lista de contatos é: $contatos <br>";
+    
+    $contatos_array = explode("-", $contatos);
+    print_r($contatos_array);
+
+    
+    # Palavras Reservadas e Funções Comuns
+    echo "Exemplo de uso da função strtoupper para converter uma string em maiúsculas: " . strtoupper($contatos) . "<br>";
+    echo "Exemplo de uso da palavra reservada require para incluir um arquivo: ";
+    //require 'arquivo.php'; // Comentar para não exibir erro;
+    echo "<br>";
+    
+    echo "Exemplo de uso de palavra reservada private para definir um método privado em uma classe: ";
+    class MinhaClasse {
+        private $atributo;
+        
+        private function metodoPrivado() {
+            // Implementação do método
+        }
+    }
+
+    echo "Exemplo de uso da constante mágica __DIR__ para obetr o diretório atual: " . __DIR__ . "<br>";
+    echo "Exemplo de uso da constante mágica __FILE__ para obter o caminho completo do arquivo atual: " . __FILE__ . "<br>";
 
 
 ?>
