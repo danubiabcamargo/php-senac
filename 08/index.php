8cf542332dfec3d85bff684ebe13093665f0ec61
Danubia@senac <danubiabcamargo@gmail.com>
2024-03-27 22:03:59 -0300

    aula 08

---
+++ 08/index.php
@@ -0,0 +1,13 @@
+<?php
+/*exercicios sobre if else while repetições */
+
+    # Estruturas de Repetição em PHP
+
+    /* Exemplo 1: FOR */
+    echo "Exemplo 1: FOR <br>";
+    for ($i = 1; $i <= 5; $i++){
+        echo "Número $i <br>";
+    }
+
+
+?>
